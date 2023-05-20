1611ba1c0ecd3bf75ac78c762771a248213f1ae9
Ayushi Gupta <ayushig2304@gmail.com>
2023-05-20 15:29:19 +0530

    change in src

--- components/stylecomponents/Header.styled.jsx
+++ components/stylecomponents/Header.styled.jsx
@@ -24,3 +24,21 @@ export const StyledUL = styled.ul`
 export const StyledRUL = styled.ul`
   list-style: none;
 `;
+
+
+export const StyleSpan = styled.span`
+  color: ${(props) => (props.color ? props.color : "#000")};
+`;
+
+export const StyledButton = styled.button`
+  color: ${(props) => (props.color ? props.color : "#810000")};
+  background: ${(props) => (props.color ? props.color : "transparent")};
+  padding: 0.5rem 1rem;
+  border-radius: 5px;
+  border: 1px solid #810000;
+  transition: all 0.5s ease;
+  &:hover {
+    color: #fff;
+    background-color: #810000;
+  }
+`;
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,52 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Layout from "../components/Common/Layout";
-
+import Card from "../components/Common/LayoutComponents/Card";
+import {
+  StyleSpan,
+  StyledButton,
+} from "../components/stylecomponents/Header.styled";
+import { Col, Container, Row } from "react-bootstrap";
 export default function Home() {
+  const arr = [1, 2, 3];
   return (
     <Layout title={"Home"}>
       <div
         style={{ height: "1000px", color: "#000" }}
         className="headerBack"
       ></div>
+      <div className="news-section">
+        <h2>
+          News <StyleSpan color="#810000">Section</StyleSpan>
+        </h2>
+        <Container>
+          <Row>
+            {arr.map((i) => (
+              <Col key={i} md={4}>
+                <Card />
+              </Col>
+            ))}
+          </Row>
+          <StyledButton>See More News</StyledButton>
+        </Container>
+      </div>
+      <div className="videos-section">
+        <h2>
+          Our Popular <StyleSpan color="#fff">Videos</StyleSpan>
+        </h2>
+        <Container>
+          <Row>
+            {arr.map((i) => (
+              <Col key={i} md={4}>
+                <Card />
+              </Col>
+            ))}
+          </Row>
+          <StyledButton color="#810000" backgroundcolor="#fff">
+            See More News
+          </StyledButton>
+        </Container>
+      </div>
     </Layout>
   );
 }
